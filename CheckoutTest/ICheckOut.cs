c121ff7af7ae93de8a068d3838cdaaeceedf1527
David Plant <djp14nt@hotmail.com>
2024-07-23 08:16:46 +0100

    Changing test and code to pass in prices and discounts

--- CheckoutTest/ICheckOut.cs
+++ CheckoutTest/ICheckOut.cs
@@ -50,14 +50,21 @@ namespace CheckoutTest
     }
     public interface IDiscount
     {
+        public string DiscountItem { get; set; }
         public int DiscountQty { get; set; }
         public int DiscountAmount { get; set; }
     }
 
     public class Discount : IDiscount
     {
-
+        public string DiscountItem { get; set; }
         public int DiscountQty { get; set; }
         public int DiscountAmount { get; set; }
+        public Discount(string discountItem, int discountQty, int discountAmount)
+        {
+            DiscountItem = discountItem;
+            DiscountQty = discountQty;
+            DiscountAmount = discountAmount;
+        }
     }
 }
--- CheckoutUnitTests/UnitTest1.cs
+++ CheckoutUnitTests/UnitTest1.cs
@@ -14,8 +14,11 @@ namespace CheckoutUnitTests
             prices.Add(new PriceListItem("C", 20, 0, 0));
             prices.Add(new PriceListItem("D", 15, 0, 0));
 
+            List<IDiscount> discounts = new List<IDiscount>();
+            discounts.Add(new Discount("A", 3, 130));
+            discounts.Add(new Discount("B", 2, 45));
 
-            ICheckout checkout = new Checkout(prices);
+            ICheckout checkout = new Checkout(prices, discounts);
 
 
             //Scan items
