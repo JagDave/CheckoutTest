50e8fe5121a471c0a9f8b3c5a072f391b49b0b6b
David Plant <djp14nt@hotmail.com>
2024-07-23 07:39:08 +0100

    Changed expected to int as only using ints for prices, started on checkout class

--- CheckoutTest/ICheckOut.cs
+++ CheckoutTest/ICheckOut.cs
@@ -11,4 +11,18 @@ namespace CheckoutTest
         void Scan(string item);
         int GetTotalPrice();
     }
+    public class Checkout
+    {
+        private readonly List<PriceList> _prices;
+        public Checkout()
+        {
+            _prices = new List<PriceList>();
+            _prices.Add(new PriceList("A", 50, 3, 130));
+                /*
+                 * A	50	3 for 130
+B	30	2 for 45
+C	20	
+D	15*/
+        }
+    }
 }
--- CheckoutUnitTests/UnitTest1.cs
+++ CheckoutUnitTests/UnitTest1.cs
@@ -5,7 +5,7 @@ namespace CheckoutUnitTests
     public class UnitTest1
     {
         [Fact]
-        public void Test1(List<string> items,decimal expected)
+        public void Test1(List<string> items,int expected)
         {
             //Scan items
             ICheckout checkout = new Checkout();
