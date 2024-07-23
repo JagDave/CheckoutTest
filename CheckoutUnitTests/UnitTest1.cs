7be0b6f394bb945008d8c1c5974ef980a6ad7b86
David Plant <djp14nt@hotmail.com>
2024-07-23 07:24:10 +0100

    Setup ready for coding

---
+++ CheckoutUnitTests/UnitTest1.cs
@@ -0,0 +1,15 @@
+namespace CheckoutUnitTests
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+            //Scan items
+
+            //Calculate Discounts
+
+            //Calculate total price
+        }
+    }
+}
